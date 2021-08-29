79529d28522293cdf62fe45da3bf8c9b311d82e8
unknown <sargey7@gmail.com>
2021-08-29 10:41:15 +0300

    first commit

---
+++ lessonCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lessonCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a triangle height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a triangle width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <=height; i++)
+            {
+                for (int x = 0; x <= width; x++)
+                {
+                    if ( x <= i)
+                    {
+                        Console.Write('@');
+                        continue;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
